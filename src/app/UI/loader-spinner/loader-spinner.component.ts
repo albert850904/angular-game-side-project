4b11cd022a47a539a32fd0b5221b524c2694e7b2
Kairu <albert850904@gmail.com>
2021-10-27 11:55:21 +0800

    add loading and sync pipe

---
+++ src/app/UI/loader-spinner/loader-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-spinner',
+  templateUrl: './loader-spinner.component.html',
+  styleUrls: ['./loader-spinner.component.scss'],
+})
+export class LoaderSpinnerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
